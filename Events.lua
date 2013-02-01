5add79ee8106d5d90e0cc3d8e8ee7f9e4145ee6a
Florent POUJOL <florent.poujol@gmail.com>
2013-02-01 14:58:49 +0100

    added proper comments in Event.lua

--- Events.lua
+++ Events.lua
@@ -1,54 +1,69 @@
 Event = { events = {} }
 
---
--- Register the specified function to be called whenever the specified event will be fired.
---
-function Event.Listen(eventName, func, g) -- g for garbage
+-- Make the specified function listen to the specified event.
+-- The function will be called whenever the specified event will be fired.
+-- @param eventName The event name
+-- @param _function The function, not the function name
+function Event.Listen(eventName, _function, g) -- g for garbage
     if eventName == Event then -- when users call the function with a colon, script = EventName, eventName = script, ...
         eventName = func
         func = g
     end
+
+    local errorHead = "Event.Listen(eventName, function) : "
     
+    local varType = type(eventName)
     if eventName == nil or type(eventName) ~= "string" then
-        error("Event.Listen(eventName, function) : Argument 'eventName' is nil or not a string. Must be the event name.")
+        error(errorHead .. "Argument 'eventName' is of type '" .. varType .. "' instead of 'string'. Must be the event name.")
     end
 
-    if func == nil or type(func) ~= "function" then
-        error("Event.Listen(eventName, function) : Argument 'function' is nil or not a function. Must be the function.")
+    local varType = type(_function)
+    if _function == nil or type(func) ~= "function" then
+        error(errorHead .. "Argument '_function' is of type '" .. varType .. "' instead of 'function'. Must be the function, not the function name.")
     end
 
     if Event.events[eventName] == nil then
         Event.events[eventName] = {}
     end
 
-    table.insert(Event.events[eventName], func)
+    table.insert(Event.events[eventName], _function)
 end
+-- TODO check if a global function, registered several times for the same event is called several times
 
---
--- Make a function to stop listen any event
---
-function Event.StopListen(func, g)
-    if func == Event then
+-- Make the specified function to stop listen to the specified event.
+-- @param eventName The event name
+-- @param _function The function, not the function name
+function Event.StopListen(eventName, _function, g)
+    if eventName == Event then
+        eventName = func
         func = g
     end
+
+    local errorHead = "Event.StopListen(eventName, function) : "
     
-    if func == nil or type(func) ~= "function" then
-        error("Event.StopListen(function) : Argument 'function' is nil or not a function. Must be the function.")
+    local varType = type(eventName)
+    if eventName == nil or type(eventName) ~= "string" then
+        error(errorHead .. "Argument 'eventName' is of type '" .. varType .. "' instead of 'string'. Must be the event name.")
     end
 
-    for eventName, functions in pairs(Event.events) do
-        for i, storedFunc in ipairs(functions) do
-            if func == storedFunc then
-                table.remove(Event.events[eventName], i)
-            end
+    local varType = type(_function)
+    if _function == nil or type(func) ~= "function" then
+        error(errorHead .. "Argument '_function' is of type '" .. varType .. "' instead of 'function'. Must be the function, not the function name.")
+    end
+
+    --
+
+    for i, storedFunc in ipairs(Event.events[eventName]) do
+        if func == storedFunc then
+            table.remove(Event.events[eventName], i)
         end
     end
 end
 
---
 -- Fire the specified event transmitting along all subsequent parameters to 'eventName' if some exists. 
--- All functions that listen to this event with EventManager.Listen() will be called and receive all parameters.
---
+-- All functions that listen to this event with Event.Listen() will be called and receive all parameters.
+-- @param eventName The event name
+-- @param ... (optionnal) a list of parameters to pass along
 function Event.Fire(eventName, ...)
     if eventName == Event then
         if arg ~= nill then
@@ -59,8 +74,9 @@ function Event.Fire(eventName, ...)
         end
     end
     
+    local varType = type(eventName)
     if eventName == nil or type(eventName) ~= "string" then
-        error("Event.Fire(eventName[, parameters]) : Argument 'eventName' is nil or not a string. Must be the event name.")
+        error("Event.Fire(eventName[, parameters]) : Argument 'eventName' is of type '" .. varType .. "' instead of 'string'. Must be the event name.")
     end
     
     if Event.events[eventName] == nil then return end
@@ -69,3 +85,4 @@ function Event.Fire(eventName, ...)
         func(unpack(arg))
     end
 end
+-- TODO check proper arguments passing with behavior functions
