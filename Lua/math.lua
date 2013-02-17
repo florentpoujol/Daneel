9558234d3f96333ac580a91429fb1ca91a438688
Florent POUJOL <florent.poujol@gmail.com>
2013-02-17 11:04:52 +0100

    added StackTrace

--- Lua/math.lua
+++ Lua/math.lua
@@ -1,17 +1,22 @@
 
 -- Tell wether the provided number is an integer.
 -- @param number The number to check.
--- @param strict [optionnal] (boolean=false) If true, the function returns an error when the 'number' argument is not a number.
+-- @param strict [optionnal default=false] (boolean) If true, the function returns an error when the 'number' argument is not a number.
 function math.isinteger(number, strict)
+    Daneel.StackTrace.BeginFunction("math.isinteger", number, strict)
+    
     local argType = type(number)
     if argType ~= "number" then
         if strict ~= nil and strict == true then
             error("math.isinterger(number[, strict]) : Argument 'number' is of type '"..argType.."' instead of 'number'.")
         else
+            Daneel.StackTrace.EndFunction("math.isinteger", false)
             return false
         end
     end
 
-    return number == math.floor(number)
+    local isinteger = number == math.floor(number)
+    Daneel.StackTrace.EndFunction("math.isinteger", isinteger)
+    return isinteger
 end
 -- used in table.join()
