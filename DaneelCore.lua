b692fd54e5cdf40bcef3a82dfcba64e3b6485806
Florent POUJOL <florent.poujol@gmail.com>
2013-03-14 14:44:35 +0100

    merged Daneel scripts into DaneelCore

--- DaneelCore.lua
+++ DaneelCore.lua
@@ -1,7 +1,108 @@
-
 if Daneel == nil then
     Daneel = {}
 end
+
+
+
+----------------------------------------------------------------------------------
+-- Config
+
+Daneel.defaultConfig = {
+
+    -- Objects (keys = name, value = object)
+    assetObjects = {
+        Script = Script,
+        Model = Model,
+        ModelAnimation = ModelAnimation,
+        Map = Map,
+        TileSet = TileSet,
+        Sound = Sound,
+        Scene = Scene,
+        Document = Document
+    },
+
+    componentObjects = {
+        ScriptedBehavior = ScriptedBehavior,
+        ModelRenderer = ModelRenderer,
+        MapRenderer = MapRenderer,
+        Camera = Camera,
+        Transform = Transform,
+    },
+    
+    craftStudioObjects = {
+        GameObject = GameObject,
+        Vector3 = Vector3,
+        Quaternion = Quaternion,
+        Plane = Plane,
+        Ray = Ray,
+    },
+    
+    daneelObjects = {
+        RaycastHit = RayCastHit,
+        Component = Component,
+        Asset = Asset,
+    },
+
+    -- Cast
+    castableGameObjects = {},
+    
+    -- Triggers
+    -- list of gameObjects check for rpoximity by the triggers
+    -- filled in the TriggerableGameObject script
+    triggerableGameObjects = {},
+
+    -- list of gameObject that react to the mouse input
+    mousehoverableGameObjects = {},
+
+
+    ----------------------------------------------------------------------------------
+    -- user config
+    
+    -- list of the Scripts path as value and optionaly the script alias as the key
+    -- It enabled dynamic getters and setters on these ScriptedBehaviors
+    -- and if the alias is set, allows to acces the ScriptedBehavior as a variable on the gameObjects
+    scripts = {
+        -- "fully-qualified Script path"
+        -- or
+        -- alias = "fully-qualified Script path"
+    },
+
+    -- list of the button name you defined in the Administration>Game Controls tab of your project
+    buttons = {
+        
+    },
+
+    useStackTrace = true,
+}
+
+Daneel.defaultConfig.__index = Daneel.defaultConfig
+
+
+-- called from Daneel.Awake()
+function Daneel.defaultConfig.Init()
+    Daneel.Debug.StackTrace.BeginFunction("Daneel.defaultConfig.Init")
+    Daneel.config = table.new(Daneel.config)
+    setmetatable(Daneel.config, Daneel.defaultConfig)
+
+    -- 
+    Daneel.defaultConfig.assetTypes = table.getkeys(Daneel.defaultConfig.assetObjects)
+    Daneel.defaultConfig.componentTypes = table.getkeys(Daneel.defaultConfig.componentObjects)
+
+    local t = table.new()
+    t = t:merge(Daneel.defaultConfig.assetObjects)
+    t = t:merge(Daneel.defaultConfig.componentObjects)
+    t = t:merge(Daneel.defaultConfig.craftStudioObjects)
+    t = t:merge(Daneel.defaultConfig.daneelObjects)
+    Daneel.defaultConfig.allObjects = t
+    
+    Daneel.Debug.StackTrace.EndFunction("Daneel.defaultConfig.Init")
+end
+
+
+
+----------------------------------------------------------------------------------
+-- Utilities
+
 
 Daneel.Utilities = {}
 
@@ -248,6 +349,7 @@ Daneel.Debug.StackTrace = {
 -- @param functionName (string) The function name
 -- @param ... [optional] (mixed) Arguments received by the function
 function Daneel.Debug.StackTrace.BeginFunction(functionName, ...)
+    if Daneel.config.useStackTrace == false then return end
     local errorHead = "Daneel.Debug.StackTrace.BeginFunction(functionName[, ...]) : "
     Daneel.Debug.CheckArgType(functionName, "functionName", "string", errorHead)
 
@@ -272,6 +374,7 @@ end
 -- @param functionName (string) The function name
 -- @param ... [optional] (mixed) Variable returned by the function
 function Daneel.Debug.StackTrace.EndFunction(functionName, ...)
+    if Daneel.config.useStackTrace == false then return end
     local errorHead = "Daneel.Debug.StackTrace.EndFunction(functionName[, ...]) : "
     Daneel.Debug.CheckArgType(functionName, "functionName", "string", errorHead)
 
@@ -303,4 +406,181 @@ function Daneel.Debug.StackTrace.Print()
     end
 
     print("~~~~~ Daneel.Debug.StackTrace ~~~~~ End ~~~~~")
+end
+
+
+
+----------------------------------------------------------------------------------
+-- Events
+
+
+Daneel.Events = { events = {} }
+
+--- Make the specified function listen to the specified event.
+-- The function will be called whenever the specified event will be fired.
+-- @param eventName (string) The event name.
+-- @param _function (function, string or GameObject) The function or the gameObject name or instance.
+-- @param functionName [optional default="On[eventName]"] (string) If '_function' is a gameObject name or instance, the name of the function to send the message to
+-- @param broadcast [optional default=false] (boolean) If '_function' is a gameObject name or instance, broadcast the message to all the gameObject's childrens
+function Daneel.Events.Listen(eventName, _function, functionName, broadcast)
+    Daneel.Debug.StackTrace.BeginFunction("Daneel.Events.Listen", eventName, _function)
+    local errorHead = "Daneel.Events.Listen(eventName, function) : "
+    Daneel.Debug.CheckArgType(eventName, "eventName", "string", errorHead)
+
+    if Daneel.Events.events[eventName] == nil then
+        Daneel.Events.events[eventName] = {}
+    end
+
+    local functionType = type(_function)
+    if functionType == "function" then
+        table.insert(Daneel.Events.events[eventName], _function)
+    else
+        Daneel.Debug.CheckArgType(_function, "_function", {"string", "GameObject"}, errorHead)
+        Daneel.Debug.CheckOptionalArgType(functionName, "functionName", "string", errorHead)
+        Daneel.Debug.CheckOptionalArgType(broadcast, "broadcast", "boolean", errorHead)
+
+        local gameObject = _function
+        if functionType == "string" then
+            gameObject = GameObject.Find(_function)
+            if gameObject == nil then
+                Daneel.Debug.PrintError(errorHead.."Argument '_function' : gameObject with name '".._function.."' was not found in the scene.")
+            end
+        end
+
+        if functionName == nil then
+            functionName = "On"..eventName
+        end
+
+        if broadcast == nil then
+            broadcast = false
+        end
+
+        table.insert(Daneel.Events.events[eventName], {
+            gameObject = gameObject,
+            functionName = functionName,
+            broadcast = broadcast
+        })
+    end
+
+    Daneel.Debug.StackTrace.EndFunction("Daneel.Events.Listen")
+end
+-- TODO check if a global function, registered several times for the same event is called several times
+
+--- Make the specified function to stop listen to the specified event.
+-- @param eventName (string) The event name.
+-- @param functionOrGameObject (function, string or GameObject) The function, or the gameObject name or instance.
+function Daneel.Events.StopListen(eventName, functionOrGameObject)
+    Daneel.Debug.StackTrace.BeginFunction("Daneel.Events.StopListen", eventName, functionOrGameObject)
+    local errorHead = "Daneel.Events.StopListen(eventName, functionOrGameObject) : "
+    Daneel.Debug.CheckArgType(eventName, "eventName", "string", errorHead)
+    
+    local functionType = type(functionOrGameObject)
+    if functionType == "function" then
+        for i, storedFunc in ipairs(Daneel.Events.events[eventName]) do
+            if functionOrGameObject == storedFunc then
+                table.remove(Daneel.Events.events[eventName], i)
+                break
+            end
+        end
+    else
+        local gameObject = functionOrGameObject
+        if functionType == "string" then
+            gameObject = GameObject.Find(functionOrGameObject)
+            if gameObject == nil then
+                Daneel.Debug.PrintError(errorHead.."Argument 'functionOrGameObject' : gameObject with name '".._function.."' was not found in the scene.")
+            end
+        end
+        
+        for i, storedFunc in ipairs(Daneel.Events.events[eventName]) do
+            if gameObject == storedFunc.gameObject then
+                table.remove(Daneel.Events.events[eventName], i)
+                break
+            end
+        end
+    end
+
+    Daneel.Debug.StackTrace.EndFunction("Daneel.Events.StopListen")
+end
+
+--- Fire the specified event transmitting along all subsequent parameters to 'eventName' if some exists. 
+-- All functions that listen to this event with Daneel.Events.Listen() will be called and receive all parameters.
+-- @param eventName (string) The event name.
+-- @param ... [optional] a list of parameters to pass along.
+function Daneel.Events.Fire(eventName, ...)
+    if arg == nil then
+        Daneel.Debug.StackTrace.BeginFunction("Daneel.Events.Fire", eventName, nil)
+        arg = {}
+    else
+        Daneel.Debug.StackTrace.BeginFunction("Daneel.Events.Fire", eventName, unpack(arg))
+    end
+    
+    Daneel.Debug.CheckArgType(eventName, "eventName", "string", "Daneel.Events.Fire(eventName[, parameters]) : ")
+    
+    if Daneel.Events.events[eventName] == nil then 
+        Daneel.Debug.StackTrace.EndFunction("Daneel.Events.Fire")
+        return
+    end
+    
+    for i, func in ipairs(Daneel.Events.events[eventName]) do
+        local functionType = type(func)
+        if functionType == "function" then
+            func(unpack(arg))
+        elseif functionType == "table" then
+            if func.gameObject ~= nil then
+                if func.broadcast then
+                    gameObject:BroadcastMessage(func.functionName, arg)
+                else
+                    gameObject:SendMessage(func.functionName, arg)
+                end
+            else
+                table.remove(Daneel.Events.events[eventName], i)
+            end
+        else
+            -- func is nil (a priori), function has been destroyed (probably was a public function on a destroyed ScriptedBehavior)
+            table.remove(Daneel.Events.events[eventName], i)
+        end
+    end
+
+    Daneel.Debug.StackTrace.EndFunction("Daneel.Events.Fire")
+end
+
+
+
+----------------------------------------------------------------------------------
+--
+
+
+function Daneel.Awake()
+    Daneel.Debug.StackTrace.BeginFunction("Daneel.Awake")
+
+    -- Config
+    Daneel.defaultConfig.Init()
+        
+    -- Helpers functions
+    Asset.Init()
+    Component.Init()
+    GameObject.Init()
+    
+    Daneel.Debug.StackTrace.EndFunction("Daneel.Awake")
+end
+
+function Daneel.Start()
+
+end 
+
+function Daneel.Update()
+    -- fire an event whenever a registered button is pressed
+    for i, buttonName in ipairs(Daneel.config.buttons) do
+        if CraftStudio.Input.IsButtonDown(buttonName) then
+            Daneel.Events.Fire("On"..buttonName:ucfirst().."ButtonDown")
+        end
+
+        if CraftStudio.Input.WasButtonJustPressed(buttonName) then
+            Daneel.Events.Fire("On"..buttonName:ucfirst().."ButtonDown")
+        end
+
+        if CraftStudio.Input.WasButtonJustPressed(buttonName) then
+            Daneel.Events.Fire("On"..buttonName:ucfirst().."ButtonDown")
+        end
+    end
 end
