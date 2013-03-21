8fcef998627380e034d0e25dbd8da0e2b913085b
Florent POUJOL <florent.poujol@gmail.com>
2013-03-21 12:22:28 +0100

    fixed CameraMouseOver

--- Behaviors/CameraMouseOver.lua
+++ Behaviors/CameraMouseOver.lua
@@ -1,25 +1,30 @@
+-- Add this script as ScriptedBehavior on your camera
 
 function Behavior:Update()
-    local ray = self.gameObject.camera:Ray()
+    local ray = self.gameObject.camera:CreateRay(CraftStudio.Input.GetMousePosition())
 
     for i, gameObject in ipairs(Daneel.config.mousehoverableGameObjects) do
-        if ray:IntersectsGameObject(gameObject) then
-            -- the mouse pointer is over the gameObject
-            -- the action will depend on if this is the first time it hovers the gameObject
-            -- or if it was already over it the last frame
-            -- also on the user's input (clicks) while it hovers the gameObject
-            if gameObject.onMouseOver == true then
-                gameObject:SendMessage("OnMouseOver")
+        if gameObject ~= nil then
+            if ray:IntersectsGameObject(gameObject) ~= nil then
+                -- the mouse pointer is over the gameObject
+                -- the action will depend on if this is the first time it hovers the gameObject
+                -- or if it was already over it the last frame
+                -- also on the user's input (clicks) while it hovers the gameObject
+                if gameObject.onMouseOver == true then
+                    gameObject:SendMessage("OnMouseOver")
+                else
+                    gameObject.onMouseOver = true
+                    gameObject:SendMessage("OnMouseEnter")
+                end
             else
-                gameObject.onMouseOver = true
-                gameObject:SendMessage("OnMouseEnter")
+                -- was the gameObject still hovered the last frame ?
+                if gameObject.onMouseOver == true then
+                    gameObject.onMouseOver = false
+                    gameObject:SendMessage("OnMouseExit")
+                end
             end
         else
-            -- was the gameObject still hovered the last frame ?
-            if gameObject.onMouseOver == true then
-                gameObject.onMouseOver = false
-                gameObject:SendMessage("OnMouseExit")
-            end
+            table.remove(Daneel.config.mousehoverableGameObjects, i)
         end
-    end -- end for
+    end
 end
