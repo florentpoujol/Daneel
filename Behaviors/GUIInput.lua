5ff6069ddde4596b5b89d9f3bd2c77d8febe8e23
Florent POUJOL <florent.poujol@gmail.com>
2013-04-11 16:27:39 +0200

    moved event function creation in a local function

--- Behaviors/GUIInput.lua
+++ Behaviors/GUIInput.lua
@@ -1,20 +1,58 @@
 
 -- Behavior for Daneel.GUI.Checkbox
 
-function Behavior:Start()
-	Daneel.Events.Listen("OnLeftMouseButtonJustPressed", self.gameObject)
-
-	for key, value in ipairs(Daneel.config.inputKeys) do
+local B = Behavior
+local function CreateFunctions()
+	-- create function to catch the event for each inputKeys
+	for key, value in pairs(Daneel.config.inputKeys) do
 		-- the button name may be the key or the value
-		if type(value) == "string" then
-			Daneel.Events.Listen("On"..value:ucfirst().."ButtonJustPressed", self.gameObject)
-		elseif type(key) == "string" then
-			Daneel.Events.Listen("On"..key:ucfirst().."ButtonJustPressed", self.gameObject)
+		local button = value
+		local comb = {}
+
+		if type(key) == "string" then
+			button = key
+			comb = value
+		end
+
+		local Button = button:ucfirst()
+
+		B["On"..Button.."ButtonJustPressed"] = function(self)
+			if self.element.focused then
+				table.print(comb)
+				for button, value in pairs(comb) do
+					if CraftStudio.Input.IsButtonDown(button) then
+						self.element:UpdateLabel(value)
+						return
+					end
+				end
+
+				-- upper case, lower case
+				if CraftStudio.Input.IsButtonDown("LeftShift") then
+					self.element:UpdateLabel(Button)
+				else
+					self.element:UpdateLabel(button)
+				end
+			end
 		end
 	end
+end
 
+
+function Behavior:Start()
+	Daneel.Events.Listen("OnLeftMouseButtonJustPressed", self.gameObject)
 	Daneel.Events.Listen("OnLeftArrowButtonJustPressed", self.gameObject)
 	Daneel.Events.Listen("OnRightArrowButtonJustPressed", self.gameObject)
+	Daneel.Events.Listen("OnDeleteButtonJustPressed", self.gameObject)
+
+	for key, value in pairs(Daneel.config.inputKeys) do
+		-- the button name may be the key or the value
+        if type(key) == "string" then
+            value = key
+        end
+        Daneel.Events.Listen("On"..value:ucfirst().."ButtonJustPressed", self.gameObject)
+	end
+
+	CreateFunctions()
 end
 
 
@@ -41,43 +79,8 @@ end
 function Behavior:OnDeleteButtonJustPressed()
 	if self.element.focused then
 		self.element:UpdateLabel("Delete")
-		self.element:SetCursorPosition(-1, true)
 	end
 end
 
-
--- create function to catch the event for each inputKeys
-for key, value in ipairs(Daneel.config.inputKeys) do
-	-- the button name may be the key or the value
-	local button = key
-	local comb = {}
-
-	if type(value) == "string" then
-		button = value
-	elseif type(key) == "string" then
-		comb = value
-	end
-
-	local Button = button:ucfirst()
-
-	Behavior["On"..Button.."ButtonJustPressed"] = function(self)
-		if self.element.focused then
-			
-			for button, value in pairs(comb) do
-				if CraftStudio.Input.IsButtonDown(button) then
-					self.element:UpdateLabel(value)
-					return
-				end
-			end
-
-			-- upper case, lower case
-			if CraftStudio.Input.IsButtonDown("LeftShift") then
-				self.element:UpdateLabel(Button)
-			else
-				self.element:UpdateLabel(button)
-			end
-		end
-	end
-end
 
 
