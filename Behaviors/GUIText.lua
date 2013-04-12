9d05365aeae926341bb545fd25967f9402bfa483
Florent POUJOL <florent.poujol@gmail.com>
2013-04-12 17:55:05 +0200

    changed the indentation of GUIText behavior

--- Behaviors/GUIText.lua
+++ Behaviors/GUIText.lua
@@ -2,34 +2,34 @@
 -- Behavior for Daneel.GUI.Text that are also buttons
 
 function Behavior:Start()
-	Daneel.Events.Listen("OnLeftMouseButtonJustReleased", self.gameObject)
+    Daneel.Events.Listen("OnLeftMouseButtonJustReleased", self.gameObject)
 end
 
 function Behavior:OnLeftMouseButtonJustReleased()
-	if self.gameObject.onMouseOver == true then
-		self.gameObject:SendMessage("OnClick", {element = self.element})
+    if self.gameObject.onMouseOver == true then
+        self.gameObject:SendMessage("OnClick", {element = self.element})
 
-		if type(self.element.onClick) == "function"then
-			self.element:onClick()
-		end
+        if type(self.element.onClick) == "function"then
+            self.element:onClick()
+        end
     end
 end
 
 -- call the mouse hoverable callbacks
 function Behavior:OnMouseEnter()
-	if type(self.element.onMouseEnter) == "function" then
-		self.element:onMouseEnter()
-	end
+    if type(self.element.onMouseEnter) == "function" then
+        self.element:onMouseEnter()
+    end
 end
 
 function Behavior:OnMouseOver()
-	if type(self.element.onMouseOver) == "function" then
-		self.element:onMouseOver()
-	end
+    if type(self.element.onMouseOver) == "function" then
+        self.element:onMouseOver()
+    end
 end
 
 function Behavior:OnMouseExit()
-	if type(self.element.onMouseExit) == "function" then
-		self.element:onMouseExit()
-	end
+    if type(self.element.onMouseExit) == "function" then
+        self.element:onMouseExit()
+    end
 end
