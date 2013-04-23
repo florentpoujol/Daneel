5d110b012bae3a32b5a5302b8d9e6e0bb52b3fdc
Florent POUJOL <florent.poujol@gmail.com>
2013-04-23 18:26:28 +0200

    fixed CheckBox ScriptedBehavior

--- Behaviors/GUI/Checkbox.lua
+++ Behaviors/GUI/Checkbox.lua
@@ -1,14 +1,6 @@
 
--- Behavior for Daneel.GUI.Checkbox
+-- Behavior for Daneel.GUI.CheckBox
 
--- the gameObject has already registered to the "OnLeftMouseButtonJustReleased" event in GUI/Interactive
-function Behavior:OnLeftMouseButtonJustReleased()
-	-- onMouseOver comes from Daneel/Behavior/CameraMouseOver
-	-- because Checkboxes are also mousehoverable gameObjects
-	if self.gameObject.onMouseOver == true then
-		-- checkbox is overed by the mouse
-		-- and the left mouse button has been pressed
-		-- > change the state of the element
-		self.element:SwitchState()
-	end
+function Behavior:OnClick()
+	self.element:SwitchState()
 end
