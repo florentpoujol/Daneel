b8070c0288c6da3f26a827aec35ca467ed912e2b
Florent POUJOL <florent.poujol@gmail.com>
2013-06-12 17:06:20 +0200

    added GUI.Hud behavior

--- Behaviors/HUD.lua
+++ Behaviors/HUD.lua
@@ -1,4 +1,31 @@
 
+-- Behavior for Daneel.GUI.Hud component.
+-- Only add to a gameObject while in the scene editor.
+
+-- Public properties :
+-- positionX (number) [default=-1]
+-- positionY (number) [default=-1]
+-- localPositionX (number) [default=-1]
+-- localPositionY (number) [default=-1]
+-- layer (number) [default=-1]
+-- localLayer (number) [default=-1]
+
 function Behavior:Start()
-    setmetatable()
+	if self.gameObject.checkBox == nil then
+		local params = {}
+		if self.positionX >= 0 and self.positionY >= 0 then
+			params.position = Vector2.New(self.positionX, self.positionY)
+		end
+		if self.localPositionX >= 0 and self.localPositionY >= 0 then
+			params.localPosition = Vector2.New(self.localPositionX, self.localPositionY)
+		end
+		if self.layer >= 0  then
+			params.layer = self.layer
+		end
+		if self.localLayer >= 0  then
+			params.localLayer = self.localLayer
+		end
+
+		self.gameObject:AddComponent("CheckBox", params)
+	end
 end
