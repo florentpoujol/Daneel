c12421f0a59fe27e7033a50cf819b7408bf218d0
Florent POUJOL <florent.poujol@gmail.com>
2013-07-15 22:40:48 +0200

    Added "checkInterval" property to triggers and renammed "layers" property to "tags"

--- Behaviors/Trigger.lua
+++ Behaviors/Trigger.lua
@@ -1,57 +1,69 @@
 
 -- Public properties :
 -- range (number) [default= 0]
--- layers (string) [default=""]
--- isStatic (boolean) [default=false]
+-- tags (string) [default=""]
+-- checkInterval (number) [default=10]
 
 
 function Behavior:Awake()
-    self.gameObjectsInRange = table.new() -- the gameObjects that touches this trigger
-    if self.layers == "" then
-        self.layers = {}
+    self.GameObjectsInRange = {} -- the gameObjects that touches this trigger
+
+    if self.tags:trim() == "" then
+        self.tags = {}
     else
-        self.layers = self.layers:split(",", true)
+        self.tags = self.tags:split( ",", true )
     end
+
+    self.frameCount = 0
 end
 
 
 function Behavior:Update()
-    if self.range <= 0 or self.isStatic == true or #self.layers == 0 then
+    self.frameCount = self.frameCount + 1
+
+    if self.range <= 0 or self.checkInterval < 1 or #self.tags == 0 then
         return
     end
-    local triggerPosition = self.gameObject.transform.position
+
+    if self.checkInterval > 1 and self.frameCount % self.checkInterval ~= 0 then
+        return
+    end
+
+    local triggerPosition = self.gameObject.transform:GetPosition()
     
-    for i, layer in ipairs(self.layers) do
+    for i, layer in ipairs(self.tags) do
         local gameObjects = GameObject.tags[layer]
         if gameObjects ~= nil then
             
             for i = #gameObjects, i, -1 do
                 local gameObject = gameObjects[i]
-                if gameObject ~= nil and gameObject.inner ~= nil then
+                if gameObject ~= nil and gameObject.isDestroyed ~= true then
+
                     if 
                         gameObject ~= self.gameObject and 
-                        Vector3.Distance(gameObject.transform.position, triggerPosition) < self.range 
+                        Vector3.Distance( gameObject.transform:GetPosition(), triggerPosition ) < self.range 
                     then
-                        if self.gameObjectsInRange:containsvalue(gameObject) == false then
+                        if not table.containsvalue( self.GameObjectsInRange, gameObject ) then
                             -- just entered the trigger
-                            self.gameObjectsInRange:insert(gameObject)
-                            Daneel.Event.Fire(gameObject, "OnTriggerEnter", self.gameObject)
-                            Daneel.Event.Fire(self.gameObject, "OnTriggerEnter", gameObject)
+                            table.insert( self.GameObjectsInRange, gameObject )
+                            Daneel.Event.Fire( gameObject, "OnTriggerEnter", self.gameObject )
+                            Daneel.Event.Fire( self.gameObject, "OnTriggerEnter", gameObject )
                         else
                             -- already in this trigger
-                            Daneel.Event.Fire(gameObject, "OnTriggerStay", self.gameObject)
-                            Daneel.Event.Fire(self.gameObject, "OnTriggerStay", gameObject)
+                            Daneel.Event.Fire( gameObject, "OnTriggerStay", self.gameObject )
+                            Daneel.Event.Fire( self.gameObject, "OnTriggerStay", gameObject )
                         end
                     else
                         -- was the gameObject still in this trigger the last frame ?
-                        if self.gameObjectsInRange:containsvalue(gameObject) == true then
-                            self.gameObjectsInRange:removevalue(gameObject)
-                            Daneel.Event.Fire(gameObject, "OnTriggerExit", self.gameObject)
-                            Daneel.Event.Fire(self.gameObject, "OnTriggerExit", gameObject)
+                        if table.containsvalue( self.GameObjectsInRange, gameObject ) then
+                            table.removevalue( self.GameObjectsInRange, gameObject )
+                            Daneel.Event.Fire( gameObject, "OnTriggerExit", self.gameObject )
+                            Daneel.Event.Fire( self.gameObject, "OnTriggerExit", gameObject )
                         end
                     end
+
                 else
-                    table.remove(gameObjects)
+                    table.remove( gameObjects )
                 end
             end
 
@@ -60,7 +72,7 @@ function Behavior:Update()
 end
 
 --- Get the gameObjets that are closer than the trigger's range.
--- @param tags [optional] (string or table) The tags(s) the gameObjects must have. If nil, it uses the trigger's layer(s).
+-- @param tags [optional] (string or table) The tags(s) the gameObjects must have. If nil, it uses the trigger's tags(s).
 -- @param range [optional] (number) The range within which to pick the gameObjects. If nil, it uses the trigger's range.
 -- @return (table) The list of the gameObjects in range (empty if none in range).
 function Behavior:GetGameObjectsInRange( tags, range )
@@ -70,39 +82,36 @@ function Behavior:GetGameObjectsInRange( tags, range )
     end
 
     local errorHead = "Trigger:GetGameObjectsInRange( [tags, range] ) : "
-    Daneel.Debug.CheckOptionalArgType( tags, "tags", {"string", "table"}, errorHead)
+    Daneel.Debug.CheckOptionalArgType( tags, "tags", {"string", "table"}, errorHead )
 
     if tags == nil then
-        if self.isStatic == false then
-            if self.gameObjectsInRange == nil then -- happens when called before Awake is called
-                self.gameObjectsInRange = {}
-            end
-            return self.gameObjectsInRange
-        end
-        tags = self.layers
+        tags = self.tags
     end
     if type( tags ) == "string" then
         tags = tags:split( ",", true )
     end
 
-    range = Daneel.Debug.CheckOptionalArgType( range, "range", "number", errorHead, self.range)
+    range = Daneel.Debug.CheckOptionalArgType( range, "range", "number", errorHead, self.range )
     
-    local gameObjectsInRange = table.new()
-
-    local triggerPosition = self.gameObject.transform.position
+    local gameObjectsInRange = {}
+    local triggerPosition = self.gameObject.transform:GetPosition()
+    
     for i, layer in ipairs( tags ) do
         local gameObjects = GameObject.tags[ layer ]
         if gameObjects ~= nil then
+
             for i, gameObject in ipairs( gameObjects ) do
                 if 
-                    gameObject ~= nil and gameObject.inner ~= nil and
+                    gameObject ~= nil and gameObject.isDestroyed ~= true and
                     gameObject ~= self.gameObject and 
-                    Vector3.Distance( gameObject.transform.position, triggerPosition ) <= self.range
+                    Vector3.Distance( gameObject.transform:GetPosition(), triggerPosition ) <= self.range
                 then
                     table.insert( gameObjectsInRange, gameObject )
                 end
             end
+
         end
     end
+
     return gameObjectsInRange
 end
