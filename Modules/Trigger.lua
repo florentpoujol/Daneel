f0927c8df6e42e29d322870753d530e7988233a1
Florent POUJOL <florent.poujol@gmail.com>
2013-09-25 18:32:36 +0200

    Updated Trigger, added Trigger:IsGameObjectInRange()

--- Modules/Trigger.lua
+++ Modules/Trigger.lua
@@ -1,111 +1,72 @@
 -- Trigger.lua
 -- Scripted behavior for triggers.
 --
--- Last modified for v1.2.1
+-- Last modified for v1.3
 -- Copyright © 2013 Florent POUJOL, published under the MIT licence.
 
 --[[PublicProperties
-range number 0
 tags string ""
+range number 0
 updateInterval number 10
 /PublicProperties]]
 
 function Behavior:Awake()
+    self.gameObject.trigger = self
     self.GameObjectsInRange = {} -- the gameObjects that touches this trigger
-
-    if self.tags:trim() == "" then
-        self.tags = {}
-    else
-        self.tags = self.tags:split( ",", true )
-    end
-
+    self.tags = self.tags:split( ",", true )
     self.frameCount = 0
-
-    if self.gameObject.modelRenderer ~= nil then
-        self.gameObject.modelRenderer:SetOpacity( 0 )
-    end
-    if self.gameObject.mapRenderer ~= nil then
-        self.gameObject.mapRenderer:SetOpacity( 0 )
-    end
-
     self.ray = Ray:New()
+    
+    -- if self.gameObject.modelRenderer ~= nil then
+    --     self.gameObject.modelRenderer:SetOpacity( 0 )
+    -- end
+    -- if self.gameObject.mapRenderer ~= nil then
+    --     self.gameObject.mapRenderer:SetOpacity( 0 )
+    -- end
 end
-
 
 function Behavior:Update()
     self.frameCount = self.frameCount + 1
+    if self.updateInterval > 1 and #self.tags > 0 and self.frameCount % self.updateInterval == 0 then
+        local triggerPosition = self.gameObject.transform:GetPosition()
+        
+        for i, layer in ipairs( self.tags ) do
+            local gameObjects = GameObject.Tags[ layer ]
+            if gameObjects ~= nil then
+                
+                for i, gameObject in pairs( gameObjects ) do
+                    local gameObject = gameObjects[ i ]
+                    if gameObject.transform ~= nil and gameObject ~= self.gameObject then
 
-    if 
-        self.updateInterval < 1 or #self.tags == 0 or
-        ( self.updateInterval > 1 and self.frameCount % self.updateInterval ~= 0 )
-    then
-        return
-    end
+                        local gameObjectIsInRange = self:IsGameObjectInRange( gameObject, triggerPosition )
+                        local gameObjectWasInRange = table.containsvalue( self.GameObjectsInRange, gameObject )
 
-    local triggerPosition = self.gameObject.transform:GetPosition()
-    
-    for i, layer in ipairs( self.tags ) do
-        local gameObjects = GameObject.Tags[ layer ]
-        if gameObjects ~= nil then
-            
-            for i = #gameObjects, i, -1 do
-                local gameObject = gameObjects[ i ]
-                if gameObject ~= nil and gameObject.isDestroyed ~= true and gameObject ~= self.gameObject then
-
-                    local gameObjectIsInTrigger = false
-                    local gameObjectPosition = gameObject.transform:GetPosition()
-                    local directionToTrigger = triggerPosition - gameObjectPosition
-                    local distanceToTriggerSquared = directionToTrigger:SqrLength()
-                    length = directionToTrigger:length()
-
-                    if self.range > 0 and distanceToTriggerSquared < self.range^2 then
-                        gameObjectIsInTrigger = true
-
-                    elseif self.range <= 0 then
-                        self.ray.position = gameObjectPosition
-                        self.ray.direction = directionToTrigger:Normalized() -- ray from the gameObject to the trigger
-                        local distance = nil
-
-                        if self.gameObject.modelRenderer ~= nil then
-                            distance = ray:IntersectsModelRenderer( self.gameObject.modelRenderer )
-                        elseif self.gameObject.mapRenderer ~= nil then
-                            distance = ray:IntersectsMapRenderer( self.gameObject.mapRenderer )
-                        end
-
-                        if distance ~= nil and distance^2 > distanceToTriggerSquared then
-                            -- distance from the GO to the model or map is superior to the distance to the trigger
-                            -- that means the GO is inside of the mode/map
-                            -- the ray goes throught the GO origin before intersecting the map 
-                            gameObjectIsInTrigger = true
-                        end
-                    end
-
-
-                    if gameObjectIsInTrigger then
-                        if not table.containsvalue( self.GameObjectsInRange, gameObject ) then
-                            -- just entered the trigger
-                            table.insert( self.GameObjectsInRange, gameObject )
-                            Daneel.Event.Fire( gameObject, "OnTriggerEnter", self.gameObject )
-                            Daneel.Event.Fire( self.gameObject, "OnTriggerEnter", gameObject )
+                        if gameObjectIsInRange then
+                            if gameObjectWasInRange then
+                                -- already in this trigger
+                                Daneel.Event.Fire( gameObject, "OnTriggerStay", self.gameObject )
+                                Daneel.Event.Fire( self.gameObject, "OnTriggerStay", gameObject )
+                            else
+                                -- just entered the trigger
+                                table.insert( self.GameObjectsInRange, gameObject )
+                                Daneel.Event.Fire( gameObject, "OnTriggerEnter", self.gameObject )
+                                Daneel.Event.Fire( self.gameObject, "OnTriggerEnter", gameObject )
+                            end
                         else
-                            -- already in this trigger
-                            Daneel.Event.Fire( gameObject, "OnTriggerStay", self.gameObject )
-                            Daneel.Event.Fire( self.gameObject, "OnTriggerStay", gameObject )
+                            -- was the gameObject still in this trigger the last frame ?
+                            if table.containsvalue( self.GameObjectsInRange, gameObject ) then
+                                table.removevalue( self.GameObjectsInRange, gameObject )
+                                Daneel.Event.Fire( gameObject, "OnTriggerExit", self.gameObject )
+                                Daneel.Event.Fire( self.gameObject, "OnTriggerExit", gameObject )
+                            end
                         end
+
                     else
-                        -- was the gameObject still in this trigger the last frame ?
-                        if table.containsvalue( self.GameObjectsInRange, gameObject ) then
-                            table.removevalue( self.GameObjectsInRange, gameObject )
-                            Daneel.Event.Fire( gameObject, "OnTriggerExit", self.gameObject )
-                            Daneel.Event.Fire( self.gameObject, "OnTriggerExit", gameObject )
-                        end
+                        table.remove( gameObjects, i )
                     end
-
-                else
-                    table.remove( gameObjects )
                 end
-            end
 
+            end
         end
     end
 end
@@ -115,6 +76,7 @@ end
 -- @param range [optional] (number) The range within which to pick the gameObjects. If nil, it uses the trigger's range.
 -- @return (table) The list of the gameObjects in range (empty if none in range).
 function Behavior:GetGameObjectsInRange( tags, range )
+    Daneel.Debug.StackTrace.BeginFunction( "Trigger:GetGameObjectsInRange", tags, range )
     if type( tags ) == "number" then
         range = tags
         tags = nil
@@ -135,48 +97,66 @@ function Behavior:GetGameObjectsInRange( tags, range )
     local gameObjectsInRange = {}
     local triggerPosition = self.gameObject.transform:GetPosition()
     
-    for i, layer in ipairs( tags ) do
-        local gameObjects = GameObject.Tags[ layer ]
+    for i, tag in pairs( tags ) do
+        local gameObjects = GameObject.Tags[ tag ]
         if gameObjects ~= nil then
 
-            for i, gameObject in ipairs( gameObjects ) do
-                if gameObject ~= nil and gameObject.isDestroyed ~= true and gameObject ~= self.gameObject then 
-                    
-                    local gameObjectIsInTrigger = false
-                    local gameObjectPosition = gameObject.transform:GetPosition()
-                    local directionToTrigger = triggerPosition - gameObjectPosition
-                    local distanceToTriggerSquared = directionToTrigger:SqrLength()
-
-                    if self.range > 0 and distanceToTriggerSquared < self.range^2 then
-                        gameObjectIsInTrigger = true
-
-                    elseif self.range <= 0 then
-                        local ray = Ray:New( gameObjectPosition, directionToTrigger ) -- ray from the gameObject to the trigger
-                        local distance = nil
-
-                        if gameObject.modelRenderer ~= nil then
-                            distance = ray:IntersectsModelRenderer( gameObject.modelRenderer )
-                        elseif gameObject.mapRenderer ~= nil then
-                            distance = ray:IntersectsMapRenderer( gameObject.mapRenderer )
-                        end
-
-                        if distance ~= nil and distance^2 > distanceToTriggerSquared then
-                            -- distance from the GO to the model or map is superior to the distance to the trigger
-                            -- that means the GO is inside of the mode/map
-                            -- the ray goes throught the GO origin before intersecting the map 
-                            gameObjectIsInTrigger = true
-                        end
-                    end
-
-                    if gameObjectIsInTrigger then
-                        table.insert( gameObjectsInRange, gameObject )
-                    end
-
+            for i, gameObject in pairs( gameObjects ) do
+                if 
+                    gameObject.transform ~= nil and gameObject ~= self.gameObject and
+                    self:IsGameObjectInRange( gameObject, triggerPosition ) and
+                    not table.containsvalue( gameObjectsInRange, gameObject )
+                then
+                    table.insert( gameObjectsInRange, gameObject )
                 end
             end
-
         end
     end
 
+    Daneel.Debug.StackTrace.EndFunction()
     return gameObjectsInRange
+end
+
+--- Tell wether the provided game object is in range of the trigger.
+-- @param gameObject (GameObject) The gameObject.
+-- @param triggerPosition (Vector3) [optional] The current position of trigger.
+-- @return (boolean) True or false.
+function Behavior:IsGameObjectInRange( gameObject, triggerPosition )
+    Daneel.Debug.StackTrace.BeginFunction( "Trigger:IsGameObjectInRange", gameObject, triggerPosition )
+    local errorHead = "Behavior:IsGameObjectInRange( gameObject[, triggerPosition] )"
+    Daneel.Debug.CheckArgType( gameObject, "gameObject", "GameObject", errorHead )
+    Daneel.Debug.CheckOptionalArgType( triggerPosition, "triggerPosition", "Vector3", errorHead )
+    if triggerPosition == nil then
+        triggerPosition = self.gameObject.transform:GetPosition()
+    end 
+
+    local gameObjectIsInTrigger = false
+    local gameObjectPosition = gameObject.transform:GetPosition()
+    local directionToTrigger = triggerPosition - gameObjectPosition
+    local distanceToTriggerSquared = directionToTrigger:SqrLength()
+
+    if self.range > 0 and distanceToTriggerSquared < self.range^2 then
+        gameObjectIsInTrigger = true
+
+    elseif self.range <= 0 then
+        self.ray.position = gameObjectPosition
+        self.ray.direction = directionToTrigger -- ray from the gameObject to the trigger
+        local distance = nil
+
+        if gameObject.modelRenderer ~= nil then
+            distance = self.ray:IntersectsModelRenderer( gameObject.modelRenderer )
+        elseif gameObject.mapRenderer ~= nil then
+            distance = self.ray:IntersectsMapRenderer( gameObject.mapRenderer )
+        end
+
+        if distance ~= nil and distance^2 > distanceToTriggerSquared then
+            -- distance from the GO to the model or map is superior to the distance to the trigger
+            -- that means the GO is inside of the mode/map
+            -- the ray goes throught the GO origin before intersecting the map 
+            gameObjectIsInTrigger = true
+        end
+    end
+
+    Daneel.Debug.StackTrace.EndFunction()
+    return gameObjectIsInTrigger
 end
