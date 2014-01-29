1e25034b5abf541850e73d3996017ebe0eeed396
Florent POUJOL <florent.poujol@gmail.com>
2014-01-29 11:54:49 +0100

    Prevented MouseInput:Update() to run when not necessary.

--- Mouse Input.lua
+++ Mouse Input.lua
@@ -1,7 +1,7 @@
 -- Mouse Input.lua
 -- Enable mouse interactions with game objects when added to a game object with a camera component.
 --
--- Last modified for v1.3.0
+-- Last modified for v1.3.1
 -- Copyright © 2013-2014 Florent POUJOL, published under the MIT license.
 
 MouseInput = {}
@@ -13,25 +13,49 @@ CS.DaneelModules[ "MouseInput" ] = MouseInput
 
 function MouseInput.DefaultConfig()
     return {
-        doubleClickDelay = 20 -- Maximum number of frames between two clicks of the left mouse button to be considered as a double click
+        doubleClickDelay = 20, -- Maximum number of frames between two clicks of the left mouse button to be considered as a double click
     }
 end
 MouseInput.Config = MouseInput.DefaultConfig()
+
+function MouseInput.Load()
+    MouseInput.buttonExists = { LeftMouse = false, RightMouse = false, WheelUp = false, WheelDown = false }
+    local eventList = {}
+    for buttonName, _ in pairs( MouseInput.buttonExists ) do
+        local exists = Daneel.Utilities.ButtonExists( buttonName )
+        MouseInput.buttonExists[ buttonName ] = exists
+        
+        if exists then
+            table.insert( eventList, "On"..buttonName.."ButtonWasJustPressed" )
+            if buttonName == "LeftMouse" then
+                table.insert( eventList, "OnLeftMouseButtonDown" )
+            end
+        end
+    end
+
+    MouseInput.update = false -- prevent the system to miss a click if it happens when  self.frameCount % self.updateInterval ~= 0  by forcing the update
+    
+    if #eventList > 0 then
+        Daneel.Event.Listen(
+            eventList,
+            function() MouseInput.update = true end,
+            true -- persistent listener
+        )
+    end
+end
 
 
 ----------------------------------------------------------------------------------
 
 --[[PublicProperties
 tags string ""
-updateInterval number 2
+onMouseOverInterval number 0
+updateWhenMouseMoves boolean true
 /PublicProperties]]
 
 function Behavior:Awake()
-    if not MouseInput.isLoaded then
-        if table.getvalue( _G, "MouseInputUserConfig" ) ~= nil and type( MouseInputUserConfig ) == "function" then
-            MouseInput.Config = MouseInputUserConfig()
-        end
-        MouseInput.isLoaded = true
+    if not Daneel.isLoaded then
+        Daneel.LateLoad( "MouseInput.Awake" )
     end
 
     Daneel.Debug.StackTrace.BeginFunction( "MouseInput:Awake" )
@@ -48,58 +72,52 @@ function Behavior:Awake()
     self.gameObject.mouseInput = self
     self.frameCount = 0
     self.lastLeftClickFrame = -MouseInput.Config.doubleClickDelay
-    
-    self.update = false -- prevent the system to miss a click if it happens when   self.frameCount % self.updateInterval ~= 0  by forcing the update
-    Daneel.Event.Listen(
-        { "OnLeftMouseButtonWasJustPressed", "OnLeftMouseButtonDown", "OnRightMouseButtonWasJustPressed" },
-        function() self.update = true end
-    )
-
-    self.buttonExists = { LeftMouse = false, RightMouse = false, WheelUp = false, WheelDown = false }
-    for button, exists in pairs( self.buttonExists ) do
-        self.buttonExists[ button ] = Daneel.Utilities.ButtonExists( button )
-    end
 
     Daneel.Debug.StackTrace.EndFunction()
 end
 
 function Behavior:Update()
     self.frameCount = self.frameCount + 1
-    if self.update or self.frameCount % self.updateInterval == 0 then
-        self.update = false
+    local mouseDelta = CS.Input.GetMouseDelta()
+    local mouseIsMoving = false
+    if mouseDelta.x ~= 0 or mouseDelta.y ~= 0 then
+        mouseIsMoving = true
+    end
+
+    if 
+        MouseInput.update or
+        (mouseIsMoving and self.updateWhenMouseMoves) or
+        ( self.onMouseOverInterval > 0 and self.frameCount % self.onMouseOverInterval == 0 )
+    then
+        MouseInput.update = false
 
         local leftMouseJustPressed = false
         local leftMouseDown = false
-        if self.buttonExists.LeftMouse then
+        if MouseInput.buttonExists.LeftMouse then
             leftMouseJustPressed = CS.Input.WasButtonJustPressed( "LeftMouse" )
             leftMouseDown = CS.Input.IsButtonDown( "LeftMouse" )
         end
 
         local rightMouseJustPressed = false
-        if self.buttonExists.RightMouse then
+        if MouseInput.buttonExists.RightMouse then
             rightMouseJustPressed = CS.Input.WasButtonJustPressed( "RightMouse" )
         end
 
         local wheelUpJustPressed = false
-        if self.buttonExists.WheelUp then
+        if MouseInput.buttonExists.WheelUp then
             wheelUpJustPressed = CS.Input.WasButtonJustPressed( "WheelUp" )
         end
 
         local wheelDownJustPressed = false
-        if self.buttonExists.WheelDown then
+        if MouseInput.buttonExists.WheelDown then
             wheelDownJustPressed = CS.Input.WasButtonJustPressed( "WheelDown" )
         end
         
+        --
         local doubleClick = false
         if leftMouseJustPressed then
             doubleClick = ( self.frameCount <= self.lastLeftClickFrame + MouseInput.Config.doubleClickDelay )   
             self.lastLeftClickFrame = self.frameCount
-        end
-
-        local vector = CS.Input.GetMouseDelta()
-        local drag = false
-        if vector.x ~= 0 or vector.y ~= 0 then
-            drag = true
         end
 
         local mousePosition = CS.Input.GetMousePosition()
@@ -127,7 +145,7 @@ function Behavior:Update()
                             end                           
 
                         elseif gameObject.isMouseOver then
-                            -- was the gameObject still hovered the last frame ?
+                            -- the gameObject was still hovered the last frame
                             gameObject.isMouseOver = false
                             Daneel.Event.Fire( gameObject, "OnMouseExit", gameObject )
                         end
@@ -142,7 +160,7 @@ function Behavior:Update()
                                 end
                             end
 
-                            if leftMouseDown and drag then
+                            if leftMouseDown and mouseIsMoving then
                                 Daneel.Event.Fire( gameObject, "OnDrag", gameObject )
                             end
 
