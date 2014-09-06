aab94a1a029040a9ec4ecb5bcf70adc1b1b6890a
Florent POUJOL <florent.poujol@gmail.com>
2014-09-06 10:35:54 +0200

    Fix missing/additional character when TextAreas cut lines.

--- GUI/GUI.lua
+++ GUI/GUI.lua
@@ -1055,11 +1055,11 @@ function GUI.TextArea.SetText( textArea, text )
                     if textArea.textRuler:GetTextWidth( newLine ) * textAreaScale.x > areaWidth then
                         if char == " " then
                             table.insert( lines, newLine:sub( 1, #newLine-1 ) )
-                            newLine = ""
+                            newLine = char
                         else
                             -- the end of the line is inside a word
                             -- go backward to find the first space char and cut the line there
-                            local word = char
+                            local word = ""
                             for k = #newLine, 1, -1 do
                                 local wordLetter = newLine:sub(k,k)
                                 if wordLetter == " " then
